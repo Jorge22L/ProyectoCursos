4bf502e238790c8cf0acd560893d9e16f5197421
Jorge22L <jorgeluismreyes@gmail.com>
2021-09-22 17:29:22 -0600

    Agregando clases y métodos para eliminar y editar

---
+++ Aplicacion/Curso/Eliminar.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Curso
+{
+    public class Eliminar
+    {
+        public class Ejecuta : IRequest
+        {
+            public int Id { get; set; }
+        }
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            private readonly CursosContext _context;
+            public Manejador(CursosContext context)
+            {
+                _context = context;
+            }
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var curso = await _context.tblCurso.FindAsync(request.Id);
+                if(curso == null)
+                {
+                    throw new Exception("El curso no existe");
+                }
+                _context.Remove(curso);
+                var resultado = await _context.SaveChangesAsync();
+                if(resultado > 0)
+                {
+                    return Unit.Value;
+                }
+                throw new Exception("No se pudo eliminar el curso");
+            }
+        }
+    }
+}
--- WebAPI/Controllers/CursosController.cs
+++ WebAPI/Controllers/CursosController.cs
@@ -37,5 +37,18 @@ namespace WebAPI.Controllers
         {
             return await _mediator.Send(data);
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Editar(int id, Editar.Ejecuta data)
+        {
+            data.CursoId = id;
+            return await _mediator.Send(data);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Eliminar(int id)
+        {
+            return await _mediator.Send(new Eliminar.Ejecuta { Id = id });
+        }
     }
 }
